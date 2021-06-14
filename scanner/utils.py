8ef66fcde85f5db0f7dd9e2022b94de1fe5d0cba
h0nda <41300123+h0nde@users.noreply.github.com>
2021-06-14 18:25:54 +0200

    Update utils.py

--- scanner/utils.py
+++ scanner/utils.py
@@ -43,11 +43,11 @@ def get_group_funds(group_id, proxy_addr=None):
     else:
         sock.connect(("economy.roblox.com", 443))
     sock = ssl.create_default_context().wrap_socket(sock, server_hostname="economy.roblox.com")
-    sock.send(f"GET /v1/groups/{group_id}/currency HTTP/1.1\r\nHost: economy.roblox.com\r\n\r\n")
+    sock.send(f"GET /v1/groups/{group_id}/currency HTTP/1.1\r\nHost: economy.roblox.com\r\n\r\n".encode())
     resp = sock.recv(1024**2)
 
     if resp.startswith(b"HTTP/1.1 200"):
-        data = json.loads(resp.split("\r\n\r\n", 1)[1])
+        data = json.loads(resp.split(b"\r\n\r\n", 1)[1])
         return data.get("robux")
     
     elif resp.startswith(b"HTTP/1.1 403") and b"code\":3" in resp:
