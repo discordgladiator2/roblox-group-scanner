f2cf21ddfdfc872e1053320e0aa7b95402f89cbc
h0nda <41300123+h0nde@users.noreply.github.com>
2021-06-21 11:17:13 +0200

    Update arguments.py

--- scanner/arguments.py
+++ scanner/arguments.py
@@ -17,25 +17,25 @@ def parse_range(range_string):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--threads", help="Number of threads per worker",
-                        type=int, default=50)
-    parser.add_argument("-w", "--workers", help="Number of workers",
-                        type=int, default=5)
     parser.add_argument("-r", "--range", help="Group id range",
                         type=parse_range, default="1-10000000")
-    parser.add_argument("--timeout", help="Max. time for connections and responses",
-                        type=float, default=5.0)
-    parser.add_argument("-p", "--proxy-list", help="File containg list of proxies",
-                        type=argparse.FileType("r", errors="ignore"))
-    parser.add_argument("-u", "--webhook-url", help="URL of webhook to be called when a claimable group is found",
-                        required=False)
+    parser.add_argument("-c", "--cut-off", help="Non-existent groups with IDs past this point won't be automatically skipped after the first attempt",
+                        type=int, default=None, required=False)
     parser.add_argument("-f", "--min-funds", help="Min. amount of funds in a group",
                         type=int, required=False)
     parser.add_argument("-m", "--min-members", help="Min. amount of members in a group",
                         type=int, required=False)
+    parser.add_argument("-u", "--webhook-url", help="URL of webhook to be called when a claimable group is found",
+                        required=False)
+    parser.add_argument("-p", "--proxy-list", help="File containg list of proxies",
+                        type=argparse.FileType("r", errors="ignore"))
     parser.add_argument("-n", "--no-close", help="If enabled, connections won't be closed based on responses. This is useful for proxies that switch IPs per request.",
                         action="store_true", default=False, required=False)
-    parser.add_argument("-c", "--cut-off", help="Non-existent groups with IDs past this point won't be automatically skipped after the first attempt",
-                        type=int, default=None, required=False)
+    parser.add_argument("--timeout", help="Max. time for connections and responses",
+                        type=float, default=5.0)
+    parser.add_argument("-w", "--workers", help="Number of workers",
+                        type=int, default=5)
+    parser.add_argument("-t", "--threads", help="Number of threads per worker",
+                        type=int, default=50)
     args = parser.parse_args()
     return args
